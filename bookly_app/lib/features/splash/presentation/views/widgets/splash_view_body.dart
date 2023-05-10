c00ff5a6d56eba99323e7b204782e10172c1a550
Ibrahim Atef <ibrahimatef715@gmail.com>
2023-05-10 20:46:20 +0300

    first commit

---
+++ bookly_app/lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:test/core/utils/assets.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+      ],
+    );
+  }
+}
---
+++ bookly_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:test/constants.dart';
+import 'features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
