4459794f638eaeb5d0e25a4c6e22f3eb94d2731f
Ibrahim Atef <90819588+Ibrahim653@users.noreply.github.com>
2023-05-21 02:12:18 +0200

    add custom app bar for home and add IconBroken

---
+++ bookly_app/lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+import '../../../../../core/utils/icon_broken.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.logo,
+          height: 20,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(IconBroken.Search,size: 26,),
+          )
+        ],
+      ),
+    );
+  }
+}
