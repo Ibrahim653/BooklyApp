44ffe17ac3e62b07d4269b4cd562bb60ca8fe867
Ibrahim Atef <90819588+Ibrahim653@users.noreply.github.com>
2023-05-21 06:12:46 +0300

    change listview item name

--- bookly_app/lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ bookly_app/lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:test/core/utils/assets.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.7/4,
+      aspectRatio: 2.7 / 4,
       child: Container(
-        decoration:  BoxDecoration(
-          borderRadius:BorderRadius.circular(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
           image: const DecorationImage(
-            image: AssetImage(AssetsData.test),
-            fit: BoxFit.fill
-          ),
+              image: AssetImage(AssetsData.test), fit: BoxFit.fill),
         ),
       ),
     );
