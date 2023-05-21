dd5481864876d19b8adb54983a69cd0d1ae0778f
Ibrahim Atef <90819588+Ibrahim653@users.noreply.github.com>
2023-05-21 06:11:30 +0300

    add Featured Books ListView

--- bookly_app/lib/features/home/presentation/views/widgets/home_view_body.dart
+++ bookly_app/lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:test/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:test/features/home/presentation/views/widgets/featured_books_listview.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
     );
   }
