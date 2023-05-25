d64654177b350a31ed28dafcb47a149ba4f048c9
Ibrahim Atef <ibrahimatef715@gmail.com>
2023-05-25 19:10:32 +0300

    a

--- bookly_app/lib/main.dart
+++ bookly_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:test/constants.dart';
 import 'features/splash/presentation/views/splash_view.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -15,7 +16,7 @@ class BooklyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
+        scaffoldBackgroundColor: kPrimaryColor,textTheme: GoogleFonts.montserratTextTheme()
       ),
       home: const SplashView(),
     );
