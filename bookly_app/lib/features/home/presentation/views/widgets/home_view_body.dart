33f2565072ac01e0b5c7f703f3fb1a6623a32f2f
Ibrahim Atef <ibrahimatef715@gmail.com>
2023-05-28 16:03:23 +0300

    add scrolling to home view

--- bookly_app/lib/features/home/presentation/views/widgets/home_view_body.dart
+++ bookly_app/lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -4,26 +4,38 @@ import 'package:test/core/utils/styles.dart';
 import 'package:test/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:test/features/home/presentation/views/widgets/featured_books_listview.dart';
 
-import 'best_seller_listview_item.dart';
+import 'best_seller_listview.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(height: 50),
-          Text('Best Seller', style: Styles.textStyle18),
-          SizedBox(height: 20),
-          BestSellerListViewItem(),
-        ],
+    return CustomScrollView(slivers: [
+      SliverToBoxAdapter(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: CustomAppBar(),
+            ),
+            FeaturedBooksListView(),
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Text('Best Seller', style: Styles.textStyle18),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
       ),
-    );
+      const SliverFillRemaining(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: BestSellerListView(),
+        ),
+      ),
+    ]);
   }
 }
