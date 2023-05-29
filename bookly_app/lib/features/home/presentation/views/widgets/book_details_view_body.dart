51d69e8c323a83a45db86caae3d853d38637e8ba
Ibrahim Atef <ibrahimatef715@gmail.com>
2023-05-29 17:00:20 +0300

    add title and subtitle to book details view

--- bookly_app/lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ bookly_app/lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:test/core/utils/styles.dart';
+import 'package:test/features/home/presentation/views/widgets/book_rating.dart';
 import 'custom_book_details_app_bar.dart';
 import 'custom_book_item.dart';
 
@@ -9,15 +11,31 @@ class BookDetailsViewBody extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
-            child: const CustomBookImage(),
+      child: Column(children: [
+        const CustomBookDetailsAppBar(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .2),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(height: 43),
+        const Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(height: 6),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
           ),
-        ],
-      ),
+        ),
+        const SizedBox(height: 14),
+        BookRating(),
+      ]),
     );
   }
 }
