0f2a6c11e3a13e1b4901353155aedb53c1409114
Ibrahim Atef <90819588+Ibrahim653@users.noreply.github.com>
2023-09-18 01:28:43 +0300

    add get it to our app (DI)

--- bookly_app/lib/main.dart
+++ bookly_app/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test/constants.dart';
 import 'package:test/core/utils/app_router.dart';
+import 'package:test/core/utils/service_locator.dart';
+import 'package:test/features/home/data/repos/home_repo_impl.dart';
+import 'package:test/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:test/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'core/utils/windows_scroll.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -13,13 +19,26 @@ class BooklyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      scrollBehavior: MyCustomScrollBehavior(),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<FeaturedBooksCubit>(
+          create: (BuildContext context) =>
+              FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider<NewestBooksCubit>(
+          create: (BuildContext context) =>
+              NewestBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        scrollBehavior: MyCustomScrollBehavior(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
