a0d125afbbce13863213a2929f71426657811822
Ibrahim Atef <ebrahemafroto653@gmail.com>
2024-03-23 08:43:24 +0200

    add on refresh method to home

--- bookly_app/lib/features/home/presentation/views/widgets/home_view_body.dart
+++ bookly_app/lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:test/core/utils/styles.dart';
+import 'package:test/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:test/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:test/features/home/presentation/views/widgets/featured_books_listview.dart';
 
+import '../../manager/featured_books_cubit/featured_books_cubit.dart';
 import 'best_seller_listview.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -11,34 +14,41 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const CustomScrollView(
-      physics:  BouncingScrollPhysics(),
-      slivers: [
-        
-      SliverToBoxAdapter(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: CustomAppBar(),
-            ),
-            FeaturedBooksListView(),
-            SizedBox(height: 50),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Text('Newest Books', style: Styles.textStyle18),
-            ),
-            SizedBox(height: 20),
-          ],
+    return RefreshIndicator(
+      onRefresh: () => _refreshData(context),
+      child: const CustomScrollView(physics: BouncingScrollPhysics(), slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: CustomAppBar(),
+              ),
+              FeaturedBooksListView(),
+              SizedBox(height: 50),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text('Newest Books', style: Styles.textStyle18),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
-      ),
-      SliverFillRemaining(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: BestSellerListView(),
+        SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: BestSellerListView(),
+          ),
         ),
-      ),
+      ]),
+    );
+  }
+
+  Future<void> _refreshData(BuildContext context) async {
+    await Future.wait([
+      BlocProvider.of<NewestBooksCubit>(context).fetchNewestBooks(),
+      BlocProvider.of<FeaturedBooksCubit>(context).fetchFeaturedBooks(),
     ]);
   }
 }
